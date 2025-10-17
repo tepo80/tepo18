0eed7b17c4874f9adada76c24ddca1ff79f0da90
tepo80 <ahsan.tepo98@gmail.com>
2025-10-17 03:34:05 +0330

    Update cl4.py
    hfdxx

--- cl4.py
+++ cl4.py
@@ -31,13 +31,7 @@ LINK_PATH = [
     "https://raw.githubusercontent.com/tepo80/tepo18/main/vip60.json",
     "https://raw.githubusercontent.com/tepo80/tepo18/main/vip70.json",
     "https://raw.githubusercontent.com/tepo80/tepo18/main/vip80.json",
-    "https://raw.githubusercontent.com/tepo80/tepo18/main/vip90.json",
-    # منابع اضافی
-    "https://raw.githubusercontent.com/tepo80/tepo18/main/h2.json",
-    "https://raw.githubusercontent.com/tepo80/tepo18/main/vless.json",
-    "https://raw.githubusercontent.com/tepo80/tepo18/main/vmess.json",
-    "https://raw.githubusercontent.com/tepo80/tepo18/main/trojan.json",
-    "https://raw.githubusercontent.com/tepo80/tepo18/main/ss.json"
+    "https://raw.githubusercontent.com/tepo80/tepo18/main/vip90.json"
 ]
 
 # ===================== توابع کمکی =====================
